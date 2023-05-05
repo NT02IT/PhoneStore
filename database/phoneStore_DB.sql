2891e3278b6f5e47e04cb83ccb82160acfb1f4cc
Khang <duong@DESKTOP-II5F8SO>
2023-05-05 22:24:21 +0700

    Sua SQL

--- database/phoneStore_DB.sql
+++ database/phoneStore_DB.sql
@@ -1,115 +1,322 @@
-USE [master]
-GO
+--
+-- Tạo Database
+--
 
-/****** Object:  Database [DoAn]    Script Date: 5/5/2023 8:31:45 PM ******/
-CREATE DATABASE [DoAn]
- CONTAINMENT = NONE
- ON  PRIMARY 
-( NAME = N'DSSV', FILENAME = N'E:\Programing System\MSSQL15.SQLEXPRESS\MSSQL\DATA\DSSV.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
- LOG ON 
-( NAME = N'DSSV_log', FILENAME = N'E:\Programing System\MSSQL15.SQLEXPRESS\MSSQL\DATA\DSSV_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
- WITH CATALOG_COLLATION = DATABASE_DEFAULT
+CREATE DATABASE DoAn
 GO
+USE DoAn
 
-IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
-begin
-EXEC [DoAn].[dbo].[sp_fulltext_database] @action = 'enable'
-end
+--
+-- Cấu trúc bảng CT_PHIEUNHAP
+--
+
+CREATE TABLE CT_PHIEUNHAP (
+	MaSP varchar(10) not null,
+	MaPN varchar(10) not null,
+	SoLuong int not null,
+	ThanhTien int not null,
+	DonGiaNhap int not null
+)
+
+--
+-- Đổ dữ liệu cho bảng CT_PHIEUNHAP
+--
+
+
+
+--
+-- Cấu trúc bảng CT_SP
+--
+
+CREATE TABLE CT_SP (
+	MaSP varchar(10) not null,
+	MaLoai varchar(10) not null,
+	MaHang varchar(10) not null,
+	MoTaSP varchar(50) not null
+)
+
+-- 
+-- Đổ dữ liệu cho bảng CT_SP
+--
+
+
+
+--
+-- Cấu trúc bảng CTHD
+--
+
+CREATE TABLE CTHD (
+	MaSP varchar(10) not null,
+	MaHD varchar(10) not null,
+	DonGia int not null,
+	SoLuong int not null,
+	ThanhTien int not null
+)
+
+--
+-- Đổ dữ liệu cho bảng CTHD
+--
+
+
+
+--
+-- Cấu trúc bảng CTKM
+--
+
+CREATE TABLE CTKM (
+	MaKM varchar(10) not null,
+	MaHD varchar(10) not null,
+	TenKM varchar(20) not null,
+	NgayBD date not null,
+	NgayKT date not null,
+	PhanTramGiam int not null
+)
+
+-- 
+-- Đổ dữ liệu cho bảng CTKM
+--
+
+
+
+--
+-- Cấu trúc bảng HANG_SX
+--
+
+CREATE TABLE HANG_SX (
+	MaHang varchar(10) not null,
+	TenHang varchar(20) not null
+)
+
+--
+-- Đổ dữ liệu cho bảng HANG_SX
+--
+
+
+
+--
+-- Cấu trúc bảng HOADON
+--
+
+CREATE TABLE HOADON (
+	MaHD varchar(10) not null,
+	MaNV varchar(10) not null,
+	MaKH varchar(10) not null,
+	NgayLap date not null,
+	TongTien int not null,
+	TienGiam int not null,
+	ConLai int not null
+)
+
+--
+-- Đổ dữ liệu cho bảng HOADON
+--
+
+
+--
+-- Cấu trúc bảng KHACHHANG
+--
+
+CREATE TABLE KHACHHANG (
+	MaKH varchar(10) not null,
+	Ten varchar(10) not null,
+	HoLot varchar(20) not null,
+	DiaChi varchar(20) not null,
+	SDT varchar(10) not null
+)
 GO
+ALTER TABLE KHACHHANG
+	ADD Username VARCHAR(50) not null,
+	    Pass VARCHAR(50) not null;
 
-ALTER DATABASE [DoAn] SET ANSI_NULL_DEFAULT OFF 
+-- 
+-- Đổ dữ liệu cho bảng KHACHHANG
+--
+
+
+
+
+--
+-- Cấu trúc bảng LOAI_SP
+--
+
+CREATE TABLE LOAI_SP (
+	MaLoai varchar(10) not null,
+	TenLoai varchar(10) not null
+)
+
+-- 
+-- Đổ dữ liệu cho bảng LOAI_SP
+-- 
+
+
+
+--
+-- Cấu trúc bảng NHACUNGCAP
+--
+
+CREATE TABLE NHACUNGCAP (
+	MaNCC varchar(10) not null,
+	TenNCC varchar(20) not null,
+	DiaChi varchar(20) not null,
+	SDT varchar(10) not null
+)
+
+--
+-- Đổ dữ liệu cho  varbảng NHACUNGCAP
+--
+
+
+
+--
+-- Cấu trúc bảng NHANVIEN
+--
+
+CREATE TABLE NHANVIEN (
+	MaNV varchar(10) not null,
+	Ten varchar(10) not null,
+	HoLot varchar(20) not null,
+	DiaChi varchar(20) not null,
+	SDT varchar(10) not null,
+	Luong int not null
+)
 GO
+ALTER TABLE NHANVIEN
+	ADD Username VARCHAR(50) not null,
+	    Pass VARCHAR(50) not null;
 
-ALTER DATABASE [DoAn] SET ANSI_NULLS OFF 
-GO
+--
+-- Đổ dữ liệu cho bảng NHANVIEN
+--
 
-ALTER DATABASE [DoAn] SET ANSI_PADDING OFF 
-GO
 
-ALTER DATABASE [DoAn] SET ANSI_WARNINGS OFF 
-GO
 
-ALTER DATABASE [DoAn] SET ARITHABORT OFF 
-GO
+-- 
+-- Cấu trúc bảng PHIEUNHAP
+--
 
-ALTER DATABASE [DoAn] SET AUTO_CLOSE OFF 
-GO
+CREATE TABLE PHIEUNHAP (
+	MaPN varchar(10) not null,
+	NgayLap date not null,
+	TongTien int not null,
+	MaNCC varchar(10) not null,
+	MaNV varchar(10) not null
+)
 
-ALTER DATABASE [DoAn] SET AUTO_SHRINK OFF 
-GO
+-- 
+-- Đổ dữ liệu cho bảng SANPHAM
+--
 
-ALTER DATABASE [DoAn] SET AUTO_UPDATE_STATISTICS ON 
-GO
+CREATE TABLE SANPHAM (
+	MaSP varchar(10) not null,
+	TenSP varchar(20) not null,
+	SoLuong int not null,
+	DonGia int not null,
+	DonViTinh varchar(10) not null,
+	MaLoai varchar(10) not null
+)	
 
-ALTER DATABASE [DoAn] SET CURSOR_CLOSE_ON_COMMIT OFF 
-GO
+--
+-- Đổ dữ liệu cho bảng SANPHAM
+--
 
-ALTER DATABASE [DoAn] SET CURSOR_DEFAULT  GLOBAL 
-GO
 
-ALTER DATABASE [DoAn] SET CONCAT_NULL_YIELDS_NULL OFF 
-GO
+--
+-- Chỉ mục cho bảng CTHD
+--
 
-ALTER DATABASE [DoAn] SET NUMERIC_ROUNDABORT OFF 
-GO
+ALTER TABLE CTHD ADD CONSTRAINT MaSP_PK_CTHD PRIMARY KEY (MaSP)
 
-ALTER DATABASE [DoAn] SET QUOTED_IDENTIFIER OFF 
-GO
+--
+-- Chỉ mục cho bảng CTKM
+--
 
-ALTER DATABASE [DoAn] SET RECURSIVE_TRIGGERS OFF 
-GO
+ALTER TABLE CTKM ADD CONSTRAINT MaKM_PK_CTKM PRIMARY KEY (MaKM)
 
-ALTER DATABASE [DoAn] SET  DISABLE_BROKER 
-GO
+--
+-- Chỉ mục cho bảng HANG_SX
+--
 
-ALTER DATABASE [DoAn] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
-GO
+ALTER TABLE HANG_SX ADD CONSTRAINT MaHang_PK_HANG_SX PRIMARY KEY (MaHang)
 
-ALTER DATABASE [DoAn] SET DATE_CORRELATION_OPTIMIZATION OFF 
-GO
+--
+-- Chỉ mục cho bảng HOADON
+--
 
-ALTER DATABASE [DoAn] SET TRUSTWORTHY OFF 
-GO
+ALTER TABLE HOADON ADD CONSTRAINT MaHD_PK_HOADON PRIMARY KEY (MaHD)
 
-ALTER DATABASE [DoAn] SET ALLOW_SNAPSHOT_ISOLATION OFF 
-GO
+--
+-- Chỉ mục cho bảng KHACHHANG
+--
 
-ALTER DATABASE [DoAn] SET PARAMETERIZATION SIMPLE 
-GO
+ALTER TABLE KHACHHANG ADD CONSTRAINT MaKH_PK_KHACHHANG PRIMARY KEY (MaKH)
 
-ALTER DATABASE [DoAn] SET READ_COMMITTED_SNAPSHOT OFF 
-GO
+--
+-- Chỉ mục cho bảng LOAI_SP
+--
 
-ALTER DATABASE [DoAn] SET HONOR_BROKER_PRIORITY OFF 
-GO
+ALTER TABLE LOAI_SP ADD CONSTRAINT MaLoai_PK_LOAI_SP PRIMARY KEY (MaLoai)
 
-ALTER DATABASE [DoAn] SET RECOVERY SIMPLE 
-GO
+--
+-- Chỉ mục cho bảng NHACUNGCAP
+--
 
-ALTER DATABASE [DoAn] SET  MULTI_USER 
-GO
+ALTER TABLE NHACUNGCAP ADD CONSTRAINT MaNCC_PK_NCC PRIMARY KEY (MaNCC)
 
-ALTER DATABASE [DoAn] SET PAGE_VERIFY CHECKSUM  
-GO
+--
+-- Chỉ mục cho bảng NHANVIEN
+--
 
-ALTER DATABASE [DoAn] SET DB_CHAINING OFF 
-GO
+ALTER TABLE NHANVIEN ADD CONSTRAINT MaNV_PK_NHANVIEN PRIMARY KEY (MaNV)
 
-ALTER DATABASE [DoAn] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
-GO
+--
+-- Chỉ mục cho bảng PHIEUNHAP
+--
 
-ALTER DATABASE [DoAn] SET TARGET_RECOVERY_TIME = 60 SECONDS 
-GO
+ALTER TABLE PHIEUNHAP ADD CONSTRAINT MaPN_PK_PN PRIMARY KEY (MaPN)
 
-ALTER DATABASE [DoAn] SET DELAYED_DURABILITY = DISABLED 
-GO
+--
+-- Chỉ mục cho bảng SANPHAM
+--
 
-ALTER DATABASE [DoAn] SET ACCELERATED_DATABASE_RECOVERY = OFF  
-GO
+ALTER TABLE SANPHAM ADD CONSTRAINT MaSP_PK_SP PRIMARY KEY (MaSP)
 
-ALTER DATABASE [DoAn] SET QUERY_STORE = OFF
-GO
+--
+-- Các ràng buộc cho bảng CT_PHIEUNHAP
+--
 
-ALTER DATABASE [DoAn] SET  READ_WRITE 
-GO
+ALTER TABLE CT_PHIEUNHAP ADD FOREIGN KEY (MaSP) REFERENCES SANPHAM (MaSP)
+ALTER TABLE CT_PHIEUNHAP ADD FOREIGN KEY (MaPN) REFERENCES PHIEUNHAP (MaPN)
 
+--
+-- Các ràng buộc cho bảng CT_SP
+--
+
+ALTER TABLE CT_SP ADD FOREIGN KEY (MaSP) REFERENCES SANPHAM (MaSP)
+ALTER TABLE CT_SP ADD FOREIGN KEY (MaLoai) REFERENCES LOAI_SP (MaLoai)
+ALTER TABLE CT_SP ADD FOREIGN KEY (MaHang) REFERENCES HANG_SX (MaHang)
+
+--
+-- Các ràng buộc cho bảng CTHD
+--
+
+ALTER TABLE CTHD ADD FOREIGN KEY (MaSP) REFERENCES SANPHAM (MaSP)
+ALTER TABLE CTHD ADD FOREIGN KEY (MaHD) REFERENCES HOADON (MaHD)
+
+--
+-- Các ràng buộc cho bảng CTKM
+--
+
+ALTER TABLE CTKM ADD FOREIGN KEY (MaHD) REFERENCES HOADON (MaHD)
+
+--
+-- Các ràng buộc cho bảng HOADON
+--
+
+ALTER TABLE HOADON ADD FOREIGN KEY (MaNV) REFERENCES NHANVIEN (MaNV)
+ALTER TABLE HOADON ADD FOREIGN KEY (MaKH) REFERENCES KHACHHANG (MaKH)
+
+--
+-- Các ràng buộc cho bảng PHIEUNHAP
+--
+
+ALTER TABLE PHIEUNHAP ADD FOREIGN KEY (MaNCC) REFERENCES NHACUNGCAP (MaNCC)
