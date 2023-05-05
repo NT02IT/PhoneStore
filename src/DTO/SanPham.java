0e8253681a839fae0c9170e65a7995f78b4d46bd
Nguyen Tuan <lowtee.vn@gmail.com>
2023-05-05 21:25:18 +0700

    Revert "đổi maLoai int -> String"
    This reverts commit 4b0de072d87ad5bcc5ae12860486ad0ceacebe6d.

--- src/DTO/SanPham.java
+++ src/DTO/SanPham.java
@@ -1,1 +1,83 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DTO;
 
+/**
+ *
+ * @author agond
+ */
+public class SanPham {
+    private static int tongSLhang = 0; // Không kể loại sản phẩm 
+    private int soLuong, donGia, maLoai;
+    private String maSP, tenSP, donViTinh;
+
+    public SanPham() {
+        tongSLhang++;
+        maSP = "SP" + tongSLhang;
+    }
+
+    public SanPham(int soLuong, int donGia, int maLoai, String tenSP, String donViTinh) {
+        this.soLuong = soLuong;
+        this.donGia = donGia;
+        this.maLoai = maLoai;
+        this.tenSP = tenSP;
+        this.donViTinh = donViTinh;
+        tongSLhang++;
+        maSP = "SP" + tongSLhang;
+    }
+
+    public static int getTongSLhang() {
+        return tongSLhang;
+    }
+
+    public String getMaSP() {
+        return maSP;
+    }
+
+    public int getSoLuong() {
+        return soLuong;
+    }
+
+    public int getDonGia() {
+        return donGia;
+    }
+
+    public int getMaLoai() {
+        return maLoai;
+    }
+
+    public String getTenSP() {
+        return tenSP;
+    }
+
+    public String getDonViTinh() {
+        return donViTinh;
+    }
+
+    public void setMaSP(String maSP) {
+        this.maSP = maSP;
+    }
+
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+
+    public void setDonGia(int donGia) {
+        this.donGia = donGia;
+    }
+
+    public void setMaLoai(int maLoai) {
+        this.maLoai = maLoai;
+    }
+
+    public void setTenSP(String tenSP) {
+        this.tenSP = tenSP;
+    }
+
+    public void setDonViTinh(String donViTinh) {
+        this.donViTinh = donViTinh;
+    }
+    
+}
